8b60da0c8a32b92e43f3be7c70c6b6a66c488e28
20171CSE0420 <60248888+20171CSE0420@users.noreply.github.com>
2020-03-09 15:48:37 +0530

    Add files via upload

---
+++ selector.ex
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/home/Sem6B7/Desktop/logo.png">
+<style>
+ h1 {color:green; font-size:10pt}
+</style>
+</head>
+<body>
+<h1 style=color: red;font-size:12pt">hello world!!!!</h>
+<p style="color:cyan;dont-size:8pt">CSS is welcoming youu...</p>
+<p>CSS is styled</P>
+<p> comments in output</P>
+</body>
+</html>
